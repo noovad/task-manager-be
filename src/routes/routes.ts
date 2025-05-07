7c6c997d87b27bc923da28c7304f91b97191987c
Noovad <novadwynt28@gmail.com>
2025-05-07 11:04:42 +0700

    add fallback route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,4 +3,9 @@ import taskRoute from "./taskRoute";
 
 export default function registerRoutes(app: express.Application) {
     app.use('/', taskRoute);
+    app.use((req, res) => {
+        res.status(404).json({
+            message: `Cannot find ${req.originalUrl} on this server`,
+        });
+    });
 }
